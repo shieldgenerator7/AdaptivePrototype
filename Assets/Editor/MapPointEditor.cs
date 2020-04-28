39bc7b8011a211d857b3de7e812bba7751e972a9
shieldgenerator7 <shieldgenerator7@yahoo.com>
2020-04-28 18:17:22 -0400

    Assign map points to map areas

---
+++ Assets/Editor/MapPointEditor.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.U2D;
+
+[CanEditMultipleObjects]
+[CustomEditor(typeof(MapPoint))]
+public class MapPointEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        GUI.enabled = !EditorApplication.isPlaying;
+        if (GUILayout.Button("Default Location Name"))
+        {
+            foreach (Object obj in targets)
+            {
+                MapPoint mp = (MapPoint)obj;
+                mp.locationName = mp.gameObject.name;
+            }
+        }
+        if (GUILayout.Button("Default MapArea"))
+        {
+            foreach (Object obj in targets)
+            {
+                MapPoint mp = (MapPoint)obj;
+                foreach (MapArea ma in FindObjectsOfType<MapArea>())
+                {
+                    if (ma.GetComponent<Collider2D>().OverlapPoint(mp.transform.position))
+                    {
+                        mp.mapArea = ma;
+                        mp.GetComponent<SpriteRenderer>().color =
+                            Color.Lerp(
+                                ma.GetComponent<SpriteShapeRenderer>().color,
+                                Color.white,
+                                0.7f
+                                );
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/MapPoint.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapPoint : MonoBehaviour
+{
+
+    public string locationName;
+    public MapArea mapArea;
+
+    private Collider2D coll;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        coll = GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (coll.OverlapPoint(Camera.main.ScreenToWorldPoint(Input.mousePosition)))
+        {
+            if (transform.localScale.x < 1)
+            {
+                transform.localScale *= 2;
+            }
+        }
+        else
+        {
+            if (transform.localScale.x >= 1)
+            {
+                transform.localScale /= 2;
+            }
+        }
+    }
+}
