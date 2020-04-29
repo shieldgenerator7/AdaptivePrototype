5d15c0ad2f6fe2ceed66278f24e2f0f8094c867d
shieldgenerator7 <shieldgenerator7@yahoo.com>
2020-04-29 00:13:20 -0400

    Wrap the screen left and right

---
+++ Assets/Scripts/WrapBounds.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WrapBounds : MonoBehaviour
+{
+    private Collider2D coll;
+
+    private void Start()
+    {
+        coll = GetComponent<Collider2D>();
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        Vector2 diff = other.transform.position - transform.position;
+        if (Mathf.Abs(diff.x) > coll.bounds.size.x/2)
+        {
+            Vector2 pos = other.transform.position;
+            pos.x = transform.position.x - diff.x;
+            other.transform.position = pos;
+        }
+    }
+}
