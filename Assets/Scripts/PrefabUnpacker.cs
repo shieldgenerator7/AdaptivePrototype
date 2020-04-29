d6a4a07354ecb2574ffd935ce0cbe4aa92c3f307
shieldgenerator7 <shieldgenerator7@yahoo.com>
2020-04-29 00:41:13 -0400

    Add prefab unpackers to asteroid group prefabs

---
+++ Assets/Scripts/PrefabUnpacker.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Put this on a grouping prefab to split it into individual objects upon spawning
+/// </summary>
+public class PrefabUnpacker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Transform parent = transform.parent;
+        foreach(Transform t in transform)
+        {
+            t.parent = parent;
+        }
+        Destroy(gameObject);
+    }
+}
